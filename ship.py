0b1df2e3a2019d73174e2687d1b76e8b162d818e
Till-Zen <tsobioch@web.de>
2024-08-06 11:37:58 +0200

    Add class Ship to manage the player's ship

---
+++ ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Ship:
+    """A class to manage the players's ship."""
+
+    def __init__(self, ai_game) -> None:
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load image of the ship and get its border rect.
+        self.image = pygame.image.load("images/ship.png")
+        self.rect = self.image.get_rect()
+
+        # Place ship in the middle on the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current position."""
+        self.screen.blit(self.image, self.rect)
