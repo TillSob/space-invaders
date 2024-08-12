8bb323b6f21ed1e2d2c4c8ea686ab9be1b06dacc
Till-Zen <tsobioch@web.de>
2024-08-12 16:12:22 +0200

    Add ship speed variable in settings and implement its usage inside the ship

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game) -> None:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load image of the ship and get its border rect.
@@ -19,16 +20,24 @@ class Ship:
         # Place ship in the middle on the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Save a float value for the center of the ship
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+
+        # Update the value for the ship's center, instead the rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update the rect-object via self.x
+        self.rect.x = self.x
 
     def blitme(self) -> None:
         """Draw the ship at its current position."""
