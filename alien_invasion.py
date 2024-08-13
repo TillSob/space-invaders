5f55e48b9ff4478ae0741e90174c274b4247f869
Till-Zen <tsobioch@web.de>
2024-08-13 09:20:54 +0200

    Add _update_bullets method to handle bullet position (Refactoring)

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,11 +28,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            # Remove bullets that pass the upper screen border
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -68,6 +64,14 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
+        # Remove bullets that pass the upper screen border
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
