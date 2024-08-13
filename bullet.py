b321ac1b4ff1fb39965ed3c9ebc5468addc5ac3a
Till-Zen <tsobioch@web.de>
2024-08-13 09:00:45 +0200

    Add class bullet and implement its size and color in settings

---
+++ bullet.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import _Group, Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, ai_game) -> None:
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet at (0, 0) and set position afterwards
+        self.rect = pygame.Rect(
+            0, 0, self.settings.bullet_width, self.settings.bullet_height
+        )
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Save the position of the bullet as float
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update float position of bullet
+        self.y -= self.settings.bullet_speed
+        # Update position of self.rect
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
